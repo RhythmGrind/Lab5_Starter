c092f24f4ac2dfe8353cdae51faae84c01ef3669
RhythmGrind <Z001026Z@GMAIL.COM>
2023-05-05 19:03:39 -0700

    u

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,38 @@
 // explore.js
-
+const speechSynthesis = window.speechSynthesis;
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voices = speechSynthesis.getVoices();
+  for (const voice of voices) {
+    const option = document.createElement("option");
+    option.value = voice.name;
+    option.textContent = voice.name;
+    document.getElementById("voice-select").appendChild(option);
+  }
+  // Set the default voice.
+  const defaultVoice = voices[0];
+  document.getElementById("voice-select").value = defaultVoice.name;
+  // Set the event listener for the "Press to Talk" button.
+  document.getElementById("press-to-talk").addEventListener("click", speak);
+
+}
+function speak() {
+  // Get the text to speak.
+  const text = document.getElementById("text-to-speak").value;
+
+  // Get the selected voice.
+  const voice = document.getElementById("voice-select").value;
+
+  // Create a new SpeechSynthesisUtterance object.
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  // Set the voice of the utterance.
+  utterance.voice = voice;
+
+  // Speak the utterance.
+  speechSynthesis.speak(utterance);
+
+  // Change the face to open mouthed.
+  document.getElementById("smiling").src = "assets/images/smiling.png";
 }

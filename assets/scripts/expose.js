e3618f492dfc0bddc73e9c850199b22de99a3f51
RhythmGrind <Z001026Z@GMAIL.COM>
2023-05-05 18:26:16 -0700

    u

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,41 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti()
+  const selectElement = document.getElementById("horn-select");
+  const audioElement = document.querySelector("audio");
+  const imageElements = document.querySelectorAll("img");
+  const volumeElement = Number(document.getElementById("volume"));
+  const iconElement = document.querySelector("input + img");
+
+  selectElement.addEventListener("change", () => {
+    const selectedValue = selectElement.value;
+    for(const imageElement of imageElements) {
+      imageElement.src = `assets/images/${selectedValue}.svg`;
+    }
+    audioElement.src = `assets/audio/${selectedValue}.mp3`;
+  });
+
+//volume slider
+  volumeElement.addEventListener("change", () => {
+    const currentVolume = volumeElement.value;
+    let level = 3;
+    if (currentVolume == 0) {
+      level = 0;
+    } else if (currentVolume< 33) {
+      level = 1;
+    } else if (currentVolume< 67) {
+      level = 2;
+    }
+    iconElement.src = `assets/icons/volume-level-${level}.svg`;
+    audioElement.volume = currentVolume/100;
+  });
+
+  document.querySelector("button").addEventListener("click", () => {
+    audioElement.play();
+    if (document.getElementById("horn-select").value === "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
+
 }
